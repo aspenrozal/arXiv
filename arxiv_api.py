d5c280c1422d509fa8d1fc839a09f77d6c6bff63
Keakoa Ula Ula <rozal@tutanota.com>
2021-07-26 14:57:45 -0700

    Used sample code from the arXiv user manual to search for fairness AND variance. Total results needs to be configured to allow for max results. If it is too low, it will miss entries, and if it is too high it will run the API request on nothing until it hits the total results max. Need to also add published data and a filter.

--- arxiv_api.py
+++ arxiv_api.py
@@ -1,5 +1,55 @@
-import urllib.request as libreq
+import urllib, urllib.request
+import feedparser 
+import time
 
-with libreq.urlopen('http://export.arxiv.org/api/query?search_query=all:electron&start=0&max_results=1') as url:
-    r = url.read()
-print(r)
+# --------------------------------------------------------------------------------------------------------------------------------------------
+# Example from: https://static.arxiv.org/static/arxiv.marxdown/0.1/help/api/examples/python_arXiv_paging_example.txt
+# Base api query url
+base_url = 'http://export.arxiv.org/api/query?';
+
+# Search parameters
+search_query = 'all:fairness+AND+variance' # search for electron in all fields
+# published = 
+start = 0                       # start at the first result
+total_results = 10              # want 20 total results
+results_per_iteration = 5       # 5 results at a time
+wait_time = 3                   # number of seconds to wait beetween calls
+
+print ('Searching arXiv for %s' % search_query)
+
+# Original:
+for i in range(start,total_results,results_per_iteration):
+# for i in range(start,results_per_iteration):
+    
+    print ("Results %i - %i" % (i,i+results_per_iteration))
+    
+    query = 'search_query=%s&start=%i&max_results=%i' % (search_query,
+                                                         i,
+                                                        results_per_iteration)
+
+    # perform a GET request using the base_url and query
+    response = urllib.request.urlopen(base_url+query).read()
+
+    # parse the response using feedparser
+    feed = feedparser.parse(response)
+
+    # Run through each entry, and print out information
+    for entry in feed.entries:
+        print ('arxiv-id: %s' % entry.id.split('/abs/')[-1])
+        print ('Title:  %s' % entry.title)
+        # feedparser v4.1 only grabs the first author
+        print ('First Author:  %s' % entry.author)
+
+    # Remember to play nice and sleep a bit before you call
+    # the api again!
+    print ('Sleeping for %i seconds' % wait_time )
+    time.sleep(wait_time)
+
+# Response is type "bytes" converting to Str
+convStr = response.decode()
+# print(type(convStr))
+# print(convStr)
+
+# Write the results to a file
+with open('test.txt', 'w') as f:
+    f.write(convStr)
