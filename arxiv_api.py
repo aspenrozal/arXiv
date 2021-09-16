59f49dbc8a04123e30f5d32c1b91fccf482fd3eb
Keakoa Ula Ula <rozal@tutanota.com>
2021-09-16 18:19:37 -0400

    Changed the composition of the DF to move paper subcategories to columns and replace the entries with counts for a given year. Stacked bar chart is successfully outputted.

--- arxiv_api.py
+++ arxiv_api.py
@@ -3,6 +3,7 @@ import feedparser
 import time
 import pandas as pd
 import matplotlib.pyplot as plt
+import numpy as np
 # --------------------------------------------------------------------------------------------------------------------------------------------
 # Example from: https://static.arxiv.org/static/arxiv.marxdown/0.1/help/api/examples/python_arXiv_paging_example.txt
 # https://arxiv.org/search/advanced?advanced=1&terms-0-operator=AND&terms-0-term=fairness&terms-0-field=all&terms-1-operator=AND&terms-1-term=variance&terms-1-field=all&terms-2-operator=OR&terms-2-term=algorithmic+bias&terms-2-field=title&classification-physics_archives=all&classification-include_cross_list=include&date-filter_by=all_dates&date-year=&date-from_date=&date-to_date=&date-date_type=submitted_date&abstracts=show&size=50&order=-announced_date_first
@@ -15,7 +16,7 @@ search_query = '%28all:%22algorithmic+fairness%22+OR+all:%22algorithmic+bias%22+
 
 # published = 
 start = 0                       # start at the first result
-total_results = 10               # want x total results
+total_results = 60               # want x total results
 results_per_iteration = 5       # 5 results at a time
 wait_time = 3                   # number of seconds to wait beetween calls
 sort_type = 'submittedDate'     # submittedDate or relevance or lastUpdatedDate
@@ -45,18 +46,8 @@ for i in range(start, total_results, results_per_iteration):
     # parse the response using feedparser
     feed = feedparser.parse(response)
 
-    # count( feed.entries )
-
-    # Debugging ------------------------------------
-    # print( 'RESPONSE', type( response ) ) #Bytes
-    # print( 'RESPONSE',len(response) )
-    # print( 'FEED', type( feed ) ) #Feed object
-    # print( 'FEED', len( feed ) )
-    # ----------------------------------------------
-    
     # Run through each entry, and print out information
-    
-
+   
     for entry in feed.entries:
         # print ('arxiv-id: %s' % entry.id.split('/abs/')[-1])
         # print ('Title:  %s' % entry.title)
@@ -64,23 +55,45 @@ for i in range(start, total_results, results_per_iteration):
         # print ('First Author:  %s' % entry.author)
         # print ( 'Date: %s ' % entry.date)
 
-        metadata.append( entry.date )
-        metadata.append( entry.category )
+        metadataEntry = list()
+
+        # Adding a seperator so that we get a cleaner date without the exact time
+        sep = "-"
+
+        metadataEntry.append( entry.date.split( sep )[0] )
+        metadataEntry.append( entry.category )
+
+        metadata.append( metadataEntry )
 
     # Remember to play nice and sleep a bit before you call
     # the api again!
     print ('Sleeping for %i seconds' % wait_time )
     time.sleep(wait_time)
 
-print( "LENGTH", len(metadata) );
-print( metadata );
-# len( 'ENTRIES', feed.entries )
+# print( "LENGTH", len(metadata) )
+# print( metadata )
 
-# # Response is type "bytes" converting to Str
-# convStr = response.decode()
-# # print(type(convStr))
-# # print(convStr)
+################################################################
+# Visualization 
+# data organization: https://www.geeksforgeeks.org/pandas-groupby-count-occurrences-in-column/
+################################################################
+df = pd.DataFrame( metadata, columns = [ 'date', 'category' ] )
+# print( df )
 
-# # Write the results to a file
-# with open('test.txt', 'w') as f:
-#     f.write(convStr)
+# 
+# df3 = df.groupby( 'category' )[ 'category'].transform( 'count' )
+# print ( df3 )
+
+# List of unique values to use for naming stacks
+# print( np.unique(df.category) )
+
+# Get the counts of the categorical data to match with year
+df2 = pd.DataFrame(df.groupby( [ 'date', 'category' ] ).size() )
+print( df2 )
+
+df3 = df2.transpose().stack(level=0)
+print( "NEW", df3 )
+
+# # Plot
+df3.plot( kind = "bar", stacked = True )
+plt.show()
